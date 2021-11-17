1619777d00968be6586d34edacb4f8e9cf5eb4ae
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-11-17 11:04:28 -0600

    Add files via upload

---
+++ Sistematico 1m2(cesar vivas) prestamo/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistematico_1m2_cesar_vivas__prestamo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
