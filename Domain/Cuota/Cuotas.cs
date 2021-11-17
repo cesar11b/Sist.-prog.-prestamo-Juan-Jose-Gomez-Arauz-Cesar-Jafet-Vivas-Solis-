1d795d8effcb97ff3d6fec85c5e3edf04aafe169
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-11-17 11:47:24 -0600

    Update Cuotas.cs

--- Domain/Cuota/Cuotas.cs
+++ Domain/Cuota/Cuotas.cs
@@ -13,19 +13,21 @@ namespace Domain.Cuota
 
         public int Id { get; set; }
 
+        public Enum Tipo { get; set; }
+
         public Estado Estado { get; set; }
-
-        public Tipo_de_prestamo Tipo_De_Prestamo { get; set; }
-
 
         public DateTime FechaVencimiento { get; set; }
 
         public DateTime FechaPago { get; set; }
 
-        public decimal Principal { get; set; }
+        public Decimal principal { get; set; }
+
         public decimal Intereses { get; set; }
-        public decimal Impuestos { get; set; }
+
         public decimal Monto_Total { get; set; }
+
+
 
     }
 }
