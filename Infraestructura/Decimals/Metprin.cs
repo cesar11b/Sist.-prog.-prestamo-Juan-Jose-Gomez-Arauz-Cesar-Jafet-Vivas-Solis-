e0637f0598f690dab26c887b618a52c4a3c3b425
cesar jafet vivas solis <88453459+cesar11b@users.noreply.github.com>
2021-11-17 11:33:06 -0600

    Update Metprin.cs
    Metodos principal, interest e impuesto

--- Infraestructura/Decimals/Metprin.cs
+++ Infraestructura/Decimals/Metprin.cs
@@ -8,5 +8,51 @@ namespace Infraestructura.Decimals
 {
     public class Metprin
     {
+        
+        public decimal Principal( decimal a, decimal b)
+        {
+            decimal resultado;
+
+            resultado = a / b;
+
+            return resultado;
+
+
+        }
+
+
+        public decimal Interest( decimal a, decimal b)
+        {
+
+            decimal resultado;
+
+            resultado = a + b;
+
+            return resultado;
+
+
+        }
+
+
+
+
+
+        public decimal Impuesto(decimal Principal, decimal impuesto)
+        {
+
+            impuesto = 0.01M;
+
+            decimal resultado;
+
+            resultado = Principal * impuesto;
+
+            return resultado;
+
+
+        }
+        
+        
+        
+        
     }
 }
